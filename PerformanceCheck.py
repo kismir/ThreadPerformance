db401f673df912c347d7ed89e2416c01017d1121
kismir <kismir@users.noreply.github.com>
2017-01-20 15:19:29 +0300

    Update PerformanceCheck.py

--- PerformanceCheck.py
+++ PerformanceCheck.py
@@ -2,16 +2,18 @@ import subprocess
 import time
 from matplotlib import pyplot as plt
 
-def thread():
+########## SUBPROCESSING PART
+
+def push_task():
     p=subprocess.Popen('python task.py', shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
     return p
 
-def run(n):
+def run_sub(n):
     t=time.time()
     res=[]
     pr=[]
     for i in range(n):
-        p=thread()
+        p=push_task()
         pr.append(p)
     while len(res)!=n:
         #time.sleep(0.01)
@@ -25,12 +27,59 @@ def run(n):
     t1=time.time()
 
     return t1-t
-        
-x=range(1,17)
-y=[]
-for i in x:
-    y.append(run(i))
 
-plt.plot(x,y)
-plt.grid()
-plt.show()
+def system_subprocess_test():
+    x=range(1,17)
+    y=[]
+    for i in x:
+        y.append(run_sub(i))
+
+    plt.plot(x,y)
+    plt.grid()
+    plt.show()
+
+############# THREADING PART
+from queue import Queue
+from threading import Thread
+
+def threading_task(out):
+    t=time.time()
+    k=0
+    for i in range(10000000):
+        k=k+1
+    t1=time.time()
+    #print(t1-t)
+    out.put(t1-t)
+
+def run_thread(n):
+    t=time.time()
+    q = Queue()
+    tp=[]
+    for i in range(1,n+1):
+        t_proc = Thread(target=threading_task, args=(q,))
+        t_proc.daemon=True
+        t_proc.start()
+    a=[]
+    while len(a)!=n:
+        z=q.get()
+        if z!=None:
+            a.append(z)
+
+    t1=time.time()
+    
+    return (t1-t)
+
+def system_thread_test():
+    x=range(1,17)
+    y=[]
+    for i in x:
+        y.append(run_thread(i))
+
+    plt.plot(x,y)
+    plt.grid()
+    plt.show()
+    
+system_subprocess_test()
+system_thread_test()
+
+# multiprocessing
