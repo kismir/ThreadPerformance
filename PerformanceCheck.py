778daf1da5e9d086cf81d4efb853221df2ac13bd
kismir <kismir@users.noreply.github.com>
2017-11-04 12:19:36 +0300

    Update PerformanceCheck.py

--- PerformanceCheck.py
+++ PerformanceCheck.py
@@ -1,14 +1,41 @@
-import subprocess
 import time
 from matplotlib import pyplot as plt
+import matplotlib as mpl
+import matplotlib.ticker as tkr
 
+import subprocess
+from queue import Queue
+from threading import Thread
+from multiprocessing import Process, Pool
+from multiprocessing import Queue as QUE
 ########## SUBPROCESSING PART
+
+def func(x, pos):  # formatter function takes tick label and tick position
+    s = str(x)
+    ind = s.index('.')
+    return s[:ind] + ',' + s[ind+1:]   # change dot to comma
+
+def plt_stats():
+    mpl.rcParams['font.family'] = 'fantasy'
+    mpl.rcParams['font.fantasy'] = 'Arial'
+
+    mpl.rc('xtick', labelsize=14) 
+    mpl.rc('ytick', labelsize=14)
+
+    plt.subplots_adjust(left=0.15, right=0.97, top=0.95, bottom=0.1)
+    plt.grid(True)
+    ax = plt.gca()
+    y_format = tkr.FuncFormatter(func)  # make formatter
+    ax.yaxis.set_major_formatter(y_format)
+    #ax.xaxis.set_major_formatter(y_format)
+    #ax.set_xticks(np.arange(0,11,1))
+    ax.yaxis.set_label_coords(-0.1, 0.46)
 
 def push_task():
     p=subprocess.Popen('python task.py', shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
     return p
 
-def run_sub(n):
+def run_sub_readline(n):
     t=time.time()
     res=[]
     pr=[]
@@ -16,13 +43,27 @@ def run_sub(n):
         p=push_task()
         pr.append(p)
     while len(res)!=n:
-        #time.sleep(0.01)
         for p in pr:
-            out = p.communicate([0])[0]
-            if out == '' and p.poll() != None:
-                break
-            if out != '':
-                res.append(out)
+            out=p.stdout.readline()
+            if out!='':
+                res.append((out,p.pid))
+
+    t1=time.time()
+
+    return t1-t
+
+def run_sub_communicate(n):
+    t=time.time()
+    res=[]
+    pr=[]
+    for i in range(n):
+        p=push_task()
+        pr.append(p)
+#    while len(res)!=n:
+    for p in pr:
+        out = p.communicate([0])[0]
+        if out != '':
+            res.append(out)
 
     t1=time.time()
 
@@ -31,16 +72,19 @@ def run_sub(n):
 def system_subprocess_test():
     x=range(1,17)
     y=[]
+    y1=[]
     for i in x:
-        y.append(run_sub(i))
+        y.append(run_sub_readline(i))
+        y1.append(run_sub_communicate(i))
 
-    plt.plot(x,y)
+    plt.figure(1)
+    plt.plot(x,y,x,y1)
     plt.grid()
-    plt.show()
+    plt.show(block=False)
+
+    return ((x,y),(x,y1))
 
 ############# THREADING PART
-from queue import Queue
-from threading import Thread
 
 def threading_task(out):
     t=time.time()
@@ -48,13 +92,11 @@ def threading_task(out):
     for i in range(10000000):
         k=k+1
     t1=time.time()
-    #print(t1-t)
     out.put(t1-t)
 
 def run_thread(n):
     t=time.time()
     q = Queue()
-    tp=[]
     for i in range(1,n+1):
         t_proc = Thread(target=threading_task, args=(q,))
         t_proc.daemon=True
@@ -67,7 +109,7 @@ def run_thread(n):
 
     t1=time.time()
     
-    return (t1-t)
+    return t1-t
 
 def system_thread_test():
     x=range(1,17)
@@ -75,11 +117,93 @@ def system_thread_test():
     for i in x:
         y.append(run_thread(i))
 
+    plt.figure(2)
     plt.plot(x,y)
     plt.grid()
-    plt.show()
-    
-system_subprocess_test()
-system_thread_test()
+    plt.show(block=False)
 
-# multiprocessing
+    return (x,y)
+    
+############# MULTIPROCESSING
+
+def multiprocessing_task_q(out):
+    t=time.time()
+    k=0
+    for i in range(10000000):
+        k=k+1
+    t1=time.time()
+    out.put(t1-t)
+
+def multiprocessing_task(_):
+    t=time.time()
+    k=0
+    for i in range(10000000):
+        k=k+1
+    t1=time.time()
+
+    return t1-t
+
+def run_multiprocess_process(n):
+    t=time.time()
+    q = QUE()
+    tp=[]
+    for i in range(1,n+1):
+        t_proc = Process(target=multiprocessing_task_q, args=(q,))
+        tp.append(t_proc)
+        t_proc.start()
+    a=[]
+    while len(a)!=n:
+        z=q.get()
+        if z!=None:
+            a.append(z)
+    q=0
+    for i in tp:
+        i.join()
+
+    t1=time.time()
+
+    return t1-t
+
+def run_multiprocess_pool(n):
+    t=time.time()
+    p=Pool(n)
+    p.map(multiprocessing_task,range(n))
+    p.close()
+    p.join()
+    t1=time.time()
+
+    return t1-t
+    
+def system_multiprocess_test():
+    x=range(1,17)
+    y=[]
+    y1=[]
+    for i in x:
+        y.append(run_multiprocess_process(i))
+        y1.append(run_multiprocess_pool(i))
+
+    plt.figure(3)
+    plt.plot(x,y,x,y1)
+    plt.grid()
+    plt.show(block=False)
+
+    return (x,y),(x,y1)
+
+############## BODY
+
+if __name__ == '__main__':
+    plot1,plot2=system_subprocess_test()
+    plot3=system_thread_test()
+    plot4,plot5=system_multiprocess_test()
+    plt.figure(4)
+    plt.plot(plot1[0],plot1[1],'x--',markersize=8,mew=2)
+    plt.plot(plot2[0],plot2[1],'x--',markersize=8,mew=2)
+    plt.plot(plot3[0],plot3[1],'x--',markersize=8,mew=2)
+    plt.plot(plot4[0],plot4[1],'x--',markersize=8,mew=2)
+    plt.plot(plot5[0],plot5[1],'x--',markersize=8,mew=2)
+    plt_stats()
+    plt.xlabel('task number', fontsize=20)
+    plt.ylabel(r's', fontsize=20,rotation=0)
+    plt.legend((("subprocess_readline"),('subprocess_communicate'),('threads'),('multiprocess_process'),('multiprocess_pool')), loc='upper left', fontsize=16)
+    plt.grid(True)
+    plt.show()
